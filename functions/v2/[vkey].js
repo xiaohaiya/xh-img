2787deb621fbb876e0be93fca824043dab0d528d
小海 <50536337+xiaohaiya@users.noreply.github.com>
2024-12-09 11:39:01 +0800

    Update [vkey].js

--- functions/v2/[vkey].js
+++ functions/v2/[vkey].js
@@ -1,9 +1,13 @@
 export async function onRequestGet({ request }) {
   const { url, method, headers, body } = request
   const newUrl = new URL(url)
-  return fetch(`https://i${Math.floor(Math.random() * 4)}.wp.com/${newUrl.hostname}${newUrl.pathname.replace('v2', 'vh-img-proxy')}${newUrl.search}`, {
+  return fetch(`https://i.imgur.com/${newUrl.pathname.replace('v2', '')}`, {
     method,
-    headers,
+    headers: {
+      ...headers,
+      referer: 'https://www.vhimg.com',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
+    },
     body
   })
 }
